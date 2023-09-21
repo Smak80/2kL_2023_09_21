225719c92324fbe62aa3c0b4db45964c9c979e2c
Sergey Makletsov <smaklets@kpfu.ru>
2023-09-21 13:38:38 +0300

    Добавьте файлы проекта.

---
+++ 2kL_2023_09_21/MyClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2kL_2023_09_21
+{
+    public class MyClass
+    {
+        public int A { get; set; }
+        public MyClass(int a) {  A = a; }
+
+        public int this[int index]
+        {
+            get {
+                var ln = A.ToString().Length;
+                if (index >= 0 && index < ln) return A.ToString()[ln-index-1]-'0';
+                return 0;
+            }
+        }
+
+        public MyClass Append(MyClass c)
+        {
+            return new MyClass(int.Parse($"{this.A}{c.A}"));
+        }
+
+        public static MyClass operator +(MyClass c1, MyClass c2)
+        {
+            return new MyClass(int.Parse($"{c1.A}{c2.A}"));
+        }
+
+        public static implicit operator MyClass(int value)
+        {
+            return new MyClass(value);
+        }
+
+        public static explicit operator int(MyClass value) => value.A;
+        public static explicit operator string(MyClass value) => value.A.ToString();
+
+        public static MyClass operator -(MyClass c)
+        {
+            return new MyClass(-c.A);
+        }
+
+        public static MyClass operator +(MyClass c)
+        {
+            return new MyClass(Math.Abs(c.A));
+        }
+
+        public static bool operator ==(MyClass c1, MyClass c2) => c1.A == c2.A;
+        public static bool operator !=(MyClass c1, MyClass c2) => !(c1 == c2);
+
+        public override string ToString() => $"[{A}]";
+    }
+}
---
+++ 2kL_2023_09_21/Program.cs
@@ -0,0 +1,17 @@
+﻿using _2kL_2023_09_21;
+
+var mc1 = new MyClass(3);
+var mc2 = new MyClass(7);
+Console.WriteLine(mc1);
+Console.WriteLine(mc2);
+var mc3 = mc1 + mc2;
+Console.WriteLine(mc3);
+Console.WriteLine(-mc3);
+Console.WriteLine(+(-mc3));
+Console.WriteLine(4 + mc1 + 5);
+MyClass mc4 = 7;
+int x = (int)mc4;
+string s = (string)(mc4+10);
+Console.WriteLine(s);
+MyClass mc5 = 358;
+Console.WriteLine(mc5[2]);
